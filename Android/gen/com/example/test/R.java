50ac1b4ac0d62343cf1a8a8039c418d64213d6ef
huchen <user@user-PC15>
2015-11-20 16:37:11 +0800

    add login and friends

--- Android/gen/com/example/test/R.java
+++ Android/gen/com/example/test/R.java
@@ -26,24 +26,32 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000d;
-        public static final int bottom=0x7f080001;
-        public static final int edit=0x7f080003;
-        public static final int image1=0x7f080009;
-        public static final int image2=0x7f08000c;
-        public static final int linear_image=0x7f08000a;
-        public static final int linear_image_p=0x7f08000b;
-        public static final int linear_text=0x7f080006;
-        public static final int linear_text_p=0x7f080007;
-        public static final int list=0x7f080000;
-        public static final int pic=0x7f080002;
-        public static final int send=0x7f080004;
-        public static final int type1=0x7f080005;
-        public static final int type2=0x7f080008;
+        public static final int action_settings=0x7f080013;
+        public static final int activity_friends_list_title=0x7f080000;
+        public static final int bottom=0x7f080003;
+        public static final int cancel=0x7f080011;
+        public static final int edit=0x7f080005;
+        public static final int image1=0x7f08000b;
+        public static final int image2=0x7f08000e;
+        public static final int linear_image=0x7f08000c;
+        public static final int linear_image_p=0x7f08000d;
+        public static final int linear_text=0x7f080008;
+        public static final int linear_text_p=0x7f080009;
+        public static final int list=0x7f080002;
+        public static final int list_view=0x7f080001;
+        public static final int login=0x7f080012;
+        public static final int loginMain=0x7f08000f;
+        public static final int name=0x7f080010;
+        public static final int pic=0x7f080004;
+        public static final int send=0x7f080006;
+        public static final int type1=0x7f080007;
+        public static final int type2=0x7f08000a;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int item=0x7f030001;
+        public static final int activity_friends_list=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int item=0x7f030002;
+        public static final int login=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -51,7 +59,23 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int btn_cancel=0x7f050007;
+        public static final int btn_convert=0x7f050006;
+        public static final int btn_login=0x7f050005;
+        public static final int btn_register=0x7f050008;
+        public static final int btn_reverse=0x7f050009;
+        public static final int btn_send=0x7f050004;
+        public static final int conn_fail=0x7f05000e;
+        public static final int exit_tip=0x7f050010;
+        public static final int hello=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int no_btn=0x7f050012;
+        public static final int view_info=0x7f05000f;
+        public static final int view_ip=0x7f05000a;
+        public static final int view_name=0x7f05000c;
+        public static final int view_port=0x7f05000b;
+        public static final int view_user=0x7f05000d;
+        public static final int yes_btn=0x7f050011;
     }
     public static final class style {
         /** 
